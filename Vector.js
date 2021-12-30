634a264e96791e56df1cd2ce5d85556e95c5c1e4
Blitz <blitz04.dev@gmail.com>
2021-12-30 23:58:11 +0530

    v0.2.0

--- Vector.js
+++ Vector.js
@@ -1,10 +1,15 @@
 export class Component {
   constructor($parent) {
-    this.$parent = parseParent($parent);
+    this.$parent = this.parseParent($parent)[0] ? this.parseParent($parent)[0] : this.parseParent($parent); // Ugly workaround for not accounting for NodeList
   }
 
   render() {
-    this.$parent.innerHTML = this.template();
+    this.destroy(); 
+    this.parseTemplate();
+    this.addEventListeners();
+    Array.from(this.parsedTemplate.children[0].children).forEach((el) => {
+      this.$parent.appendChild(el);
+    });
   }
 
   updateComponent(newState) {
@@ -15,23 +20,45 @@ export class Component {
   }
 
   destroy() {
+    // As of now this completely clears the parent. Undesirable. Need workaround.
     this.$parent.innerHTML = "";
   }
-}
 
-const parseParent = (parent) => {
-  if (typeof parent === "string") {
-    return document.querySelector(parent);
+  addEventListeners() {
+    if (!this.eventListeners) return this.parsedTemplate;
+    this.eventListeners.forEach((listener) => {
+      if (Array.isArray(listener)) {
+        // Accounting for shorthand syntax of [selector, type, callback]
+        listener = {
+          selector: listener[0],
+          type: listener[1],
+          callback: listener[2]
+        }
+      }
+      let target = this.parsedTemplate.querySelector(listener.selector)
+      if (target) {
+        target.addEventListener(listener.type, listener.callback)
+      }
+    });
   }
-  return parent;
-}
 
-const parseTemplate = (template) => {
-  // TODO: setup parsing to turn <Component /> syntax into ${new Component(...props).template()}
+  parseParent (parent) {
+    if (typeof parent === "string") {
+      return document.querySelector(parent);
+    }
+    else if (typeof parent === "object") {
+      if (parent.name) {
+        return document.getElementsByName(parent.name);
+      }
+    }
+    else if (parent instanceof NodeList) {
+      return parent[0]
+    }
+    return parent;
+  }
 
-  /*
-  const component = new window[componentName]({}, this.$parent);
-  template = template.replace(match, component.template()); 
-  */
-  return template;
+  parseTemplate () {
+    let wrapped = `<div name="_Temp">${this.template()}</div>`;
+    this.parsedTemplate = new DOMParser().parseFromString(wrapped, "text/html").body;
+  } 
 }
